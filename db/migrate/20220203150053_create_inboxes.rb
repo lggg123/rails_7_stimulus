571f3ef69fd31947eaddd44d20ab49c7dbe2633a
lggg123 <machinelearner2334@gmail.com>
2022-02-04 05:50:25 -0800

    added uniqueness to database table column

--- db/migrate/20220203150053_create_inboxes.rb
+++ db/migrate/20220203150053_create_inboxes.rb
@@ -1,7 +1,9 @@
 class CreateInboxes < ActiveRecord::Migration[7.0]
   def change
+    # uniqueness validation should have unique
+    # index on database unique: true
     create_table :inboxes do |t|
-      t.string :name, null: false
+      t.string :name, null: false, index: { unique: true }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_02_03_150053) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_inboxes_on_name", unique: true
   end
 
   add_foreign_key "comments", "articles"

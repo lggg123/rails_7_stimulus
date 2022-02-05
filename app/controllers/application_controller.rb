ceb1099597133d05e5c0a33342927b9c9777d0a7
lggg123 <machinelearner2334@gmail.com>
2022-02-04 19:15:30 -0800

    user inboxes dependent destroy addition for rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-    # After devise we add this code below
-    before_action :authenticate_user!
+  # After devise we add this code below
+  before_action :authenticate_user!
 end

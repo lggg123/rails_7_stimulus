ef8c10923208b3bd853be5730344cff0f83efb54
lggg123 <machinelearner2334@gmail.com>
2022-02-04 20:26:21 -0800

    basic message associations and validations

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,8 +1,10 @@
 class Inbox < ApplicationRecord
   belongs_to :user
+  has_many :messages, dependent: :destroy
 
   MIN_NAME = 6
   MAX_NAME = 100
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: MIN_NAME..MAX_NAME }
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :inbox
   belongs_to :user
+
+  MIN_BODY = 6
+  MAX_BODY = 2000
+
+  validates :body, presence: true
+  # add uniqueness for rails 7 instead of just true we add scope
+  # validates :body, uniqueness: { scope: :inbox_id }
+  validates :name, length: { in: MIN_BODY..MAX_BODY }
 end

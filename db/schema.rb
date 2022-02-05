9cb8a521a4b51bbbf68dab907f6984b7f049923f
lggg123 <machinelearner2334@gmail.com>
2022-02-04 18:44:04 -0800

    turned off turbojs for sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_182024) do
+ActiveRecord::Schema.define(version: 2022_02_04_205104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,9 @@ ActiveRecord::Schema.define(version: 2022_02_04_182024) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["name"], name: "index_inboxes_on_name", unique: true
+    t.index ["user_id"], name: "index_inboxes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2022_02_04_182024) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "inboxes", "users"
 end
